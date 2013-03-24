bdfeeb29fa628ac607f279f94e7476f1ce1fd74f
Angela Harms <angela.harms@gmail.com>
2013-03-24 19:23:39 -0400

    use subject instead of long specs

--- spec/go_spec.rb
+++ spec/go_spec.rb
@@ -2,27 +2,19 @@ require './go.rb'
 
 describe Go do
   it "thinks the first fibonacci number is 1" do
-    go = Go.new
-    fibonacci = go.fibo
-    fibonacci[0].should == 1
+    subject.fibonacci_sequence[0].should == 1
   end
 
   it "thinks the second fibonacci number is 2" do
-    go = Go.new
-    fibonacci = go.fibo
-    fibonacci[1].should == 2
+    subject.fibonacci_sequence[1].should == 2
   end
 
   it "gives 3 for the third fibonacci number" do
-    go = Go.new
-    fibonacci = go.fibo
-    fibonacci[2].should == 3
+    subject.fibonacci_sequence[2].should == 3
   end
 
   it "gives 5 for the fourth fibonacci number" do
-    go = Go.new
-    fibonacci = go.fibo
-    fibonacci[3].should == 5
+    subject.fibonacci_sequence[3].should == 5
   end
 
 end
