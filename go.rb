994eec7a0e959f22b6e4ef728128b0c4a2ccd6fa
Angela Harms <angela.harms@gmail.com>
2013-03-24 19:35:07 -0400

    prettier

--- go.rb
+++ go.rb
@@ -1,13 +1,16 @@
 class Go
 
   def fibonacci_sequence
-    some_array = [1, 2] 
+    build_the_thing_up_to [1,2], 4000000
+  end
+
+  def build_the_thing_up_to some_array, maximum_value
     n = 2
     while some_array[n-1] <= 4000000
       add_the_previous_two_to some_array, n
       n+=1
     end
-    some_array 
+    some_array
   end
 
   def add_the_previous_two_to the_list, n
