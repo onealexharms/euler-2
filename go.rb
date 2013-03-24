e9a21ca2f089fb3cc18e639b7c90c0ab01fee13e
Angela Harms <angela.harms@gmail.com>
2013-03-24 19:43:35 -0400

    Don't keep track of n

--- go.rb
+++ go.rb
@@ -1,16 +1,15 @@
 class Go
 
   def fibonacci_sequence
-    n = 2
     some_array = [1,2]
     while some_array.last <= 4000000 
-      add_the_previous_two_to some_array, n
-      n+=1
+      add_the_previous_two_to some_array
     end
     some_array
   end
 
-  def add_the_previous_two_to the_list, n
+  def add_the_previous_two_to the_list
+    n = the_list.size
     the_list << (the_list[n-1]+the_list[n-2])
   end
   
